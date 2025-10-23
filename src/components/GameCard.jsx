745fd7fc2c0590e39952644235246367c95aed93
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-10-23 13:48:41 +0600

    all games page done

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,74 @@
+import { SquareStar, Star } from "lucide-react";
+import { Link } from "react-router";
+import slugify from "slugify";
+
+const GameCard = ({ game }) => {
+  const isEven = game.id % 2 === 0;
+
+  return (
+    <div>
+      <div
+        className={`flex items-center md:p-8 p-2 gap-8 lg:flex-row flex-col ${
+          isEven
+            ? "lg:flex-row-reverse border-t-2 border-r-2"
+            : "border-b-2 border-l-2"
+        }`}
+      >
+        <div className="lg:w-[600px] lg:h-[300px] w-full">
+          <img
+            src={game?.coverPhoto}
+            alt={game?.title}
+            className="lg:w-[600px] lg:h-[300px] brightness-75"
+          />
+        </div>
+        <div className="space-y-4">
+          <div className="flex items-center justify-between md:flex-row flex-col gap-4">
+            <h3 className="text-3xl font-semibold text-success audiowide pb-4 border-b border-success">
+              {game?.title}
+            </h3>
+            <p className="text-2xl flex gap-1 text-success">
+              {Array.from({ length: game?.ratings }, (_, i) => (
+                <SquareStar key={i} />
+              ))}
+            </p>
+          </div>
+
+          <p className="mt-4">{game?.description}</p>
+
+          <p className="text-lg">
+            Developed By :{" "}
+            <span className="text-success audiowide">{game?.developer}</span>
+          </p>
+          <div>
+            <p className="text-xl my-4 pb-2 border-b w-48">
+              Main Characters :{" "}
+            </p>
+            <div className="flex flex-col gap-4">
+              {game?.characters.map((character) => (
+                <div className="flex gap-4 items-center">
+                  <img
+                    src={character?.image}
+                    alt={character?.name}
+                    className="w-12 h-12 rounded-md"
+                  />
+                  <p className="text-white audiowide">{character?.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <Link
+            className="btn btn-success text-black border-3 w-48 border-success  hover:bg-white rounded-md font-semibold transition-colors duration-300 mt-4"
+            to={`/game-details/${slugify(game?.title, {
+              lower: true,
+              strict: true,
+            })}`}
+          >
+            View Details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameCard;
--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,7 +1,48 @@
+import { useContext } from "react";
+import AuthContext from "../contexts/AuthContext";
+import tron from "../assets/tron.jpg";
+import GameCard from "../components/GameCard";
+import Container from "../components/Container";
+
 const Games = () => {
+  const { gamesData, gameLoading } = useContext(AuthContext);
+  console.log(gamesData);
   return (
     <div>
-      <title>GameHub - Games</title>G
+      <title>GameHub - Games</title>
+      {/* Hero Section */}
+      <div
+        className="relative w-full bg-cover bg-center py-48"
+        style={{ backgroundImage: `url(${tron})` }}
+      >
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-linear-to-r from-black/80 to-[#6C63FF]/90 mix-blend-multiply"></div>
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-center space-y-4">
+          <h1 className="lg:text-5xl text-3xl font-bold audiowide text-success">
+            Filter. Explore. Conquer.
+          </h1>
+          <p className="lg:text-lg text-secondary max-w-2xl">
+            Browse through hundreds of games, dive into detailed reviews, and
+            find your next obsession. Whether you’re into fast-paced shooters,
+            immersive RPGs, or chill indie gems — there’s something here for
+            every kind of player.
+          </p>
+        </div>
+      </div>
+
+      {/* cards section */}
+      <section className="mt-24 space-y-12">
+        <h2 className="text-center text-white audiowide lg:text-3xl text-2xl">
+          Step into the arena of legends.
+        </h2>
+        <Container>
+          <div className="grid grid-cols-1 lg:gap-16 gap-12 lg:px-5 px-2">
+            {gamesData.map((game) => (
+              <GameCard key={game.id} game={game} />
+            ))}
+          </div>
+        </Container>
+      </section>
     </div>
   );
 };
